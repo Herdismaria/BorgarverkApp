1a65ab432ec4c791518ffbc26e58b48f47f71253
Herdis Maria <herdismaria@Herdiss-MacBook-Pro.local>
2017-02-15 15:35:44 +0100

    For UI updates

--- BorgarverkApp/MainActivity.cs
+++ BorgarverkApp/MainActivity.cs
@@ -15,23 +15,7 @@ namespace BorgarverkApp
 			// Set our view from the "main" layout resource
 			SetContentView(Resource.Layout.Main);
 
-			/*Spinner carSpinner = FindViewById<Spinner>(Resource.Id.carSpinner);
-			Spinner stationSpinner = FindViewById<Spinner>(Resource.Id.stationSpinner);
-
-			ArrayAdapter carAdapter = ArrayAdapter.CreateFromResource(
-				this,
-				Resource.Array.cars,
-				Android.Resource.Layout.SimpleSpinnerItem);
-			carAdapter.SetDropDownViewResource(Android.Resource.Layout.SimpleSpinnerDropDownItem);
-
-			ArrayAdapter stationAdapter = ArrayAdapter.CreateFromResource(
-				this,
-				Resource.Array.stations,
-				Android.Resource.Layout.SimpleSpinnerItem);
-			stationAdapter.SetDropDownViewResource(Android.Resource.Layout.SimpleSpinnerDropDownItem);
-
-			carSpinner.Adapter = carAdapter;
-			stationSpinner.Adapter = stationAdapter;*/
+			/*Spinner carSpinner = FindViewById<Spinner>(Resource.Id.carSpinner);*/
 		}
 	}
 }
